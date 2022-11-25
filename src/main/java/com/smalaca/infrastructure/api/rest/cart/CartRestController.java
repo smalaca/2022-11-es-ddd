c697a88864e553ca13ed86b6452a735e3dc8092c
smalaca <ab13.krakow@gmail.com>
2022-11-25 16:48:13 +0100

    remove CartCommands from CartRestController

--- src/main/java/com/smalaca/infrastructure/api/rest/cart/CartRestController.java
+++ src/main/java/com/smalaca/infrastructure/api/rest/cart/CartRestController.java
@@ -1,14 +1,11 @@
 package com.smalaca.infrastructure.api.rest.cart;
 
-import com.smalaca.command.application.cart.CartCommands;
 import com.smalaca.query.cart.CartQueries;
 
 public class CartRestController {
     private final CartQueries queries;
-    private final CartCommands commands;
 
-    public CartRestController(CartQueries queries, CartCommands commands) {
+    public CartRestController(CartQueries queries) {
         this.queries = queries;
-        this.commands = commands;
     }
 }
